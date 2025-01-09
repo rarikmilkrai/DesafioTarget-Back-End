2adfc7efd4405d28cf7922b5f6cf52e3ab16a4d7
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2025-01-09 17:04:00 -0300

    Create Desafio_3.py

---
+++ Desafio_3.py
@@ -0,0 +1,20 @@
+import json
+
+def calcular_faturamento(faturamento_diario):
+    faturamento = [dia['valor'] for dia in faturamento_diario if dia['valor'] > 0]
+    menor_valor = min(faturamento)
+    maior_valor = max(faturamento)
+    media_mensal = sum(faturamento) / len(faturamento)
+    dias_acima_da_media = sum(1 for valor in faturamento if valor > media_mensal)
+    
+    return menor_valor, maior_valor, dias_acima_da_media
+
+if __name__ == "__main__":
+    with open('faturamento.json', 'r') as file:
+        faturamento_diario = json.load(file)
+    
+    menor_valor, maior_valor, dias_acima_da_media = calcular_faturamento(faturamento_diario)
+    
+    print(f"Menor valor de faturamento: {menor_valor}")
+    print(f"Maior valor de faturamento: {maior_valor}")
+    print(f"Dias com faturamento acima da média: {dias_acima_da_media}")
